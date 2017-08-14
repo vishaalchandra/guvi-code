d10c3f77dcce81c92e78ab387aff249f1a7f8cf5
vishaalchandra <vishaal23091997@gmail.com>
2017-08-14 12:39:06 +0530

    vowel

---
+++ vowel/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char character;
+    scanf("%c",&character);
+    if(character=='a'||'A')
+    {
+        printf("vowel");
+    }
+    else if(character=='e'||'E')
+    {
+        printf("vowel");
+    }
+    else if(character=='i'||'I')
+    {
+        printf("vowel");
+    }
+    else if(character=='o'||'O')
+    {
+        printf("vowel");
+    }
+    else if(character=='u'||'U')
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("consonant");
+    }
+    return 0;
+}
