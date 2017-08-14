c223ec2eb564e8f3d064fe9f3d7881ba2efb79b7
vishaalchandra <vishaal23091997@gmail.com>
2017-08-14 23:26:09 +0530

    leap year

---
+++ leap/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int year;
+    scanf("%d",&year);
+    if(year%4==0)
+    {
+        if(year%100==0)
+        {
+            if(year%400==0)
+            {
+                printf("leap year");
+            }
+            else
+            {
+                printf("not a leap year");
+            }
+        }
+        else
+        {
+            printf("leap year");
+        }
+    }
+    else
+    {
+        printf("not a leap year");
+    }
+    return 0;
+}
