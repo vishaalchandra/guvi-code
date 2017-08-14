6c64d6fe8bdca62261b25239d1cbf859394c5dea
vishaalchandra <vishaal23091997@gmail.com>
2017-08-14 12:22:12 +0530

    new file

---
+++ pos/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+    scanf("%d",&number);
+    if(number>0)
+    {
+     printf("positive");
+    }
+    else
+    {
+        printf("negative");
+    }
+    return 0;
+}
